7687f3f484384e471ff61c4fa3617a187b7f2782
jinkshower <askthem@naver.com>
2025-09-18 22:28:20 +0900

    Time: 15 ms (84.19%), Space: 17.8 MB (88.05%) - LeetHub

--- 0567-permutation-in-string/0567-permutation-in-string.py
+++ 0567-permutation-in-string/0567-permutation-in-string.py
@@ -1,32 +1,31 @@
 class Solution:
     def checkInclusion(self, s1: str, s2: str) -> bool:
-        count1 = {}
-        count2 = {}
-
-        n = len(s1)
-        m = len(s2)
-        if n > m:
+        if len(s1) > len(s2):
             return False
 
-        for i in range(n):
-            count1[s1[i]] = 1 + count1.get(s1[i], 0)
-            count2[s2[i]] = 1 + count2.get(s2[i], 0)
+        s1_map = {}
+        for c in s1:
+            s1_map[c] = s1_map.get(c, 0) + 1
         
-        if count1 == count2:
+        n = len(s1)
+
+        s2_map = {}
+        for i in range(n):
+            s2_map[s2[i]] = s2_map.get(s2[i], 0) + 1
+        
+        if s1_map == s2_map:
             return True
 
         l = 0
-        for r in range(n, m):
-            count2[s2[r]] = 1 + count2.get(s2[r], 0)
 
-            count2[s2[l]] -= 1
-            if count2[s2[l]] == 0:
-                count2.pop(s2[l])
+        for r in range(n, len(s2)):
+            s2_map[s2[r]] = s2_map.get(s2[r], 0) + 1
+            s2_map[s2[l]] -= 1
+            if s2_map[s2[l]] == 0:
+                del s2_map[s2[l]]
+
+            if s1_map == s2_map:
+                return True
             l += 1
 
-            print(count1)
-            print(count2)
-            if count1 == count2:
-                return True
-        
         return False
