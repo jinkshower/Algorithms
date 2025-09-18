ecf7ba0a18906601ce37b4d3d4654e7d03d4d2df
jinkshower <askthem@naver.com>
2025-09-18 22:08:46 +0900

    Time: 180 ms (24.01%), Space: 17.9 MB (86.91%) - LeetHub

--- 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.py
+++ 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.py
@@ -1,6 +1,6 @@
 class Solution:
     def characterReplacement(self, s: str, k: int) -> int:
-        count = {} # hashmap
+        count = {}
         res = 0
 
         l = 0
@@ -9,6 +9,8 @@ class Solution:
             
             while (r - l + 1) - max(count.values()) > k:
                 count[s[l]] -= 1
-                l += 1
+                l +=1
+
             res = max(res, r - l + 1)
+        
         return res
